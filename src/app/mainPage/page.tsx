d581e662e573f45fad6a2b9c9c660cfa68d48f30
drh-14 <darrenh1134@gmail.com>
2024-08-10 21:44:02 -0400

    commit

---
+++ src/app/mainPage/page.tsx
@@ -0,0 +1,85 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
+import { ButtonAppBar }  from '../components';
+import { Box, Button } from '@mui/material';
+import { onAuthStateChanged, User } from  'firebase/auth';
+import { db, auth } from '../firebase';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+
+export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState<string>('');
+  const [messageList, setMessageList] = useState([`Hi! I provide information about research at Stony Brook and guide you to start getting involved with research. How can I help you today?`]);
+  const [user, setUser] = useState<User| null>(null);
+
+ useEffect(() =>{
+  const unsubscribe = onAuthStateChanged(auth, (user) =>{
+    if(user){
+      setUser(user);
+    }
+    else{
+      setUser(null);
+      router.push('/');
+    }
+  });
+
+  return () => unsubscribe();
+ },[])
+
+ const loadMessages = async () =>{
+  if(user && user.email){
+    const docRef = doc(db, 'users', user.email)
+    const docSnap = await getDoc(docRef);
+    if(docSnap.exists()){
+      const { messages } = docSnap.data();
+      messages.forEach((msg:string) => setMessageList(prevList => [...prevList, msg]));
+    }
+  }
+};
+
+ useEffect( () =>{
+  loadMessages();
+ }, []);
+
+  const sendMessage = async (e:React.FormEvent | React.KeyboardEvent) =>{
+    e.preventDefault();
+    if(message){
+    setMessageList(prevList => [...prevList, message]);
+    if(user && user.email){
+      const docRef = doc(db, 'users', user.email);
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()){
+        const { currMessages } = docSnap.data();
+        await setDoc(docRef, { messages: [...currMessages, message] })
+      }
+    }
+    setMessage('');
+    }
+  };
+
+  return (
+    <main>
+      <ButtonAppBar user = {user}></ButtonAppBar>
+      <div className = 'flex mt-32 justify-center flex-col gap-4 items-center'>
+      <Box  className = 'w-4/12 border rounded-lg border-black' sx = {{height: '70vh'}}>
+      <div className = 'flex flex-col gap-4 ml-4 mr-4 mt-4'>
+      {messageList.map((message, index) => <div key = {index} className =  {(index % 2 === 0)? 'p-4 rounded-md bg-blue-500 text-white max-w-max':
+      'p-4 rounded-md bg-green-500 text-white max-w-max ml-auto'
+      }>{message}</div>             
+      )}
+      </div>
+      </Box>
+      <div className = 'flex justify-start justify-items-start gap-4 w-4/12 p-2'>
+        <input type = 'text' value = {message} onChange={(e) => setMessage(e.target.value)} onKeyDown = {(e:React.KeyboardEvent) => {
+          if(e.key === 'Enter'){
+            sendMessage(e);
+          }
+        }} className = 'p-4 border border-black rounded-md w-full' placeholder = 'Message'></input>
+        <Button className = 'pt-1 pb-1' variant = 'contained' onClick = {sendMessage}>Send</Button>
+      </div>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,36 @@
 'use client'
+import { useRouter } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
-import { ButtonAppBar }  from './components';
 import { Box, Button } from '@mui/material';
-import { onAuthStateChanged, User } from  'firebase/auth';
-import { db, auth } from './firebase';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth, signIn } from './firebase';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState<string>('');
-  const [messageList, setMessageList] = useState([`Hi, I'm the Headstarter AI Support Agent. How can I help you today?`]);
-  const [user, setUser] = useState<User| null>(null);
-
- useEffect(() =>{
-  const unsubscribe = onAuthStateChanged(auth, (user) =>{
-    if(user){
-      setUser(user);
-    }
-    else{
-      setUser(null);
-    }
-  });
-
-  return () => unsubscribe();
- },[])
-
- const loadMessages = async () =>{
-  if(user && user.email){
-    const docRef = doc(db, 'users', user.email)
-    const docSnap = await getDoc(docRef);
-    if(docSnap.exists()){
-      const { messages } = docSnap.data();
-      messages.forEach((msg:string) => setMessageList(prevList => [...prevList, msg]));
-    }
-  }
-};
-
- useEffect( () =>{
-  loadMessages();
- }, []);
-
-  const sendMessage = async (e:React.FormEvent | React.KeyboardEvent) =>{
-    e.preventDefault();
-    if(message){
-    setMessageList(prevList => [...prevList, message]);
-    if(user && user.email){
-      const docRef = doc(db, 'users', user.email);
-      const docSnap = await getDoc(docRef);
-      if(docSnap.exists()){
-        const { currMessages } = docSnap.data();
-        await setDoc(docRef, { messages: [...currMessages, message] })
+  const router = useRouter();
+  useEffect(() =>{
+    const unsubscribe = onAuthStateChanged(auth, (user) =>{
+      if(user){
+        router.push('/mainPage');
       }
-    }
-    setMessage('');
-    }
-  };
+      
+  })
+
+   return () => unsubscribe();
+   }, []);
 
   return (
     <main>
-      <ButtonAppBar user = {user}></ButtonAppBar>
-      <div className = 'flex mt-32 justify-center flex-col gap-4 items-center'>
-      <Box  className = 'w-4/12 border rounded-lg border-black' sx = {{height: '70vh'}}>
-      <div className = 'flex flex-col gap-4 ml-4 mr-4 mt-4'>
-      {messageList.map((message, index) => <div key = {index} className =  {(index % 2 === 0)? 'p-4 rounded-md bg-blue-500 text-white max-w-max break-all':
-      'p-4 rounded-md bg-green-500 text-white max-w-max ml-auto break-all'
-      }>{message}</div>             
-      )}
+      <div className = 'flex justify-end w-full'>
+      <Button onClick = {signIn} className = 'mt-2 mr-4 p-4' variant = 'outlined'>Sign in</Button>
       </div>
-      </Box>
-      <div className = 'flex justify-start justify-items-start gap-4 w-4/12 p-2'>
-        <input type = 'text' value = {message} onChange={(e) => setMessage(e.target.value)} onKeyDown = {(e:React.KeyboardEvent) => {
-          if(e.key === 'Enter'){
-            sendMessage(e);
-          }
-        }} className = 'p-4 border border-black rounded-md w-full' placeholder = 'Message'></input>
-        <Button className = 'pt-1 pb-1' variant = 'contained' onClick = {sendMessage}>Send</Button>
+      <div className = 'flex flex-col justify-center items-center text-center gap-20 mt-24'>
+      <div className='flex justify-center text-4xl'>Introducing ResearchHelper, a chatbot to help assist you with finding research</div>
+      <div className = 'flex flex-col gap-4 text-2xl text-center'>
+      <Box>Do you go to Stony Brook and want to get involved in research? Great!</Box>
+      <Box>But sometimes it's hard... Lots of students have no idea where to start or who to find first.</Box>
       </div>
+      <div className = 'text-3xl'>If this sounds like you, then ResearchHelper can help you! Get started today.</div>
+      <Button onClick = {signIn} className = 'w-1/12 p-4' variant = 'contained'>Get started</Button>
       </div>
     </main>
   );
